f13386c4fcc20bdb1cc6a16c129a2392b3cdcf02
Michael Sweeton <mikesweeton@gmail.com>
2023-11-16 10:58:04 +0000

    ETFE-2468 revert formatting change to index controller

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -36,8 +36,7 @@ class IndexController @Inject()(override val messagesApi: MessagesApi,
                                 getData: DataRetrievalAction
                                ) extends BaseController with Logging {
 
-  def onPageLoad(ern: String, arc: String): Action[AnyContent] = {
-
+  def onPageLoad(ern: String, arc: String): Action[AnyContent] =
     (authAction(ern, arc) andThen withMovement.fromCache(arc) andThen getData).async { implicit request =>
       request.userAnswers match {
 
@@ -53,7 +52,6 @@ class IndexController @Inject()(override val messagesApi: MessagesApi,
           initialiseAndRedirect(UserAnswers(request.ern, request.arc))
       }
     }
-  }
 
   private def initialiseAndRedirect(answers: UserAnswers)(implicit hc: HeaderCarrier): Future[Result] =
     userAnswersService.set(answers).map { _ =>
