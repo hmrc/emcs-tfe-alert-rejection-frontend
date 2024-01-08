b66cf565e17f39e294f20cbdaf05dfe1d30df238
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2024-01-08 12:08:01 +0000

    [ETFE-2979] - Updated test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,9 +5,9 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val playSuffix = "-play-30"
-  val scalatestVersion = "3.2.15"
+  val scalatestVersion = "3.2.17"
   val hmrcBootstrapVersion = "8.4.0"
-  val hmrcMongoVersion = "1.6.0"
+  val hmrcMongoVersion = "1.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
@@ -21,14 +21,14 @@ object AppDependencies {
     "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"          % hmrcBootstrapVersion,
     "org.scalatest"           %%  "scalatest"                         % scalatestVersion,
     "org.scalatestplus"       %%  "scalacheck-1-17"                   % s"$scalatestVersion.0",
-    "org.scalatestplus"       %%  "mockito-4-6"                       % s"$scalatestVersion.0",
-    "org.scalatestplus.play"  %%  "scalatestplus-play"                % "5.1.0",
+    "org.scalatestplus"       %%  "mockito-5-8"                       % s"$scalatestVersion.0",
+    "org.scalatestplus.play"  %%  "scalatestplus-play"                % "7.0.1",
     "org.scalamock"           %%  "scalamock"                         % "5.2.0",
     "org.pegdown"             %   "pegdown"                           % "1.6.0",
-    "org.jsoup"               %   "jsoup"                             % "1.15.4",
+    "org.jsoup"               %   "jsoup"                             % "1.17.2",
     "org.playframework"       %%  "play-test"                         % PlayVersion.current,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"         % hmrcMongoVersion,
-    "com.vladsch.flexmark"    %    "flexmark-all"                       % "0.62.2"
+    "com.vladsch.flexmark"    %    "flexmark-all"                       % "0.64.8"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
