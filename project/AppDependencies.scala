c39bf67bef45f146bcb4e68efa83c55e18534225
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2024-02-22 09:26:12 +0000

    [ETFE-3461] play-frontend-hmrc upgrade (#37)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc$playSuffix"      %  "8.3.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc$playSuffix"      %  "8.5.0",
     "uk.gov.hmrc"             %% s"bootstrap-frontend$playSuffix"      %  hmrcBootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playSuffix"              %  hmrcMongoVersion,
     "com.google.inject"       %   "guice"                             %  "5.1.0"
