3ca12cb3d0597888c7d12d0da764f65cf99b5b54
grahampaulcook <grahampaulcook@users.noreply.github.com>
2024-08-23 13:18:17 +0100

    ETFE-4442 correct `emcsMovementsUrl`

--- test/config/AppConfigSpec.scala
+++ test/config/AppConfigSpec.scala
@@ -48,7 +48,7 @@ class AppConfigSpec extends SpecBase with FeatureSwitching {
 
     ".emcsMovementsUrl()" - {
       "must return to the new URL" in {
-        config.emcsMovementsUrl(testErn) mustBe s"http://localhost:8310/emcs/account/movements-in/$testErn"
+        config.emcsMovementsUrl(testErn) mustBe s"http://localhost:8310/emcs/account/trader/$testErn/movements"
       }
     }
 
