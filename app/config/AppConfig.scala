d9e87d6f5d4b1a92df566ee232e74ba959ef8058
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2024-10-28 10:35:27 +0000

    [ETFE-4634] Remove beta banner and point to generic feedback survey (#56)
    * [ETFE-4634] Remove beta banner and point to generic feedback survey
    
    * [ETFE-4634] Bump major version to 1

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -18,8 +18,6 @@ package config
 
 import featureswitch.core.config.FeatureSwitching
 import play.api.Configuration
-import play.api.mvc.RequestHeader
-import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import javax.inject.{Inject, Singleton}
@@ -47,9 +45,7 @@ class AppConfig @Inject()(servicesConfig: ServicesConfig, configuration: Configu
 
   private lazy val feedbackFrontendHost: String = configuration.get[String]("feedback-frontend.host")
 
-  lazy val feedbackFrontendSurveyUrl: String = s"$feedbackFrontendHost/feedback/$deskproName/beta"
-
-  private lazy val contactHost = configuration.get[String]("contact-frontend.host")
+  lazy val feedbackFrontendSurveyUrl: String = s"$feedbackFrontendHost/feedback/$deskproName"
 
   lazy val loginUrl: String = configuration.get[String]("urls.login")
 
@@ -83,9 +79,6 @@ class AppConfig @Inject()(servicesConfig: ServicesConfig, configuration: Configu
 
   def emcsMovementsUrl(ern: String): String =
     configuration.get[String]("urls.emcsTfeMovements").replace(":ern", ern)
-
-  def betaBannerFeedbackUrl(implicit request: RequestHeader): String =
-    s"$contactHost/contact/beta-feedback?service=$deskproName&backUrl=${SafeRedirectUrl(host + request.uri).encodedUrl}"
 
   def destinationOfficeSuffix: String = configuration.get[String]("constants.destinationOfficeSuffix")
 
